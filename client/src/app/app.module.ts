437298c894d0a2cde32a413d4bfed45b7f38094d
Michelle Harrop <harrop.michelle@gmail.com>
2019-09-26 15:29:13 -0400

    index page is starting to take shape

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,9 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { Routes, RouterModule }  from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { IndexComponent } from './index/index.component';
+
+const appRoutes: Routes = [
+  {path: '', component: IndexComponent}
+];
 
 @NgModule({
   declarations: [
@@ -12,7 +17,8 @@ import { IndexComponent } from './index/index.component';
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
