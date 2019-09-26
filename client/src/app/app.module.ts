03a953f8159b006d038f000d6b446ce8895593e5
Michelle Harrop <harrop.michelle@gmail.com>
2019-09-26 15:08:40 -0400

    added client and index pages, no actual functionality on them yet

---
+++ client/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { IndexComponent } from './index/index.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    IndexComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
