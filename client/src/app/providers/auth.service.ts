ebbcc90bf647c44e2777d23164f4b6ab42770f3c
Michelle Harrop <harrop.michelle@gmail.com>
2019-10-01 16:10:43 -0400

    leagues show up but with an error

--- client/src/app/providers/auth.service.ts
+++ client/src/app/providers/auth.service.ts
@@ -1,4 +1,7 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable, Subject, of } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
